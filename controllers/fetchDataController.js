54346b496236ab833c3a5b1f327567ef0d5cfa7e
passy4ucj <ojinnnakapascal@gmail.com>
2022-06-17 08:31:56 +0100

    Date and time format

--- controllers/fetchDataController.js
+++ controllers/fetchDataController.js
@@ -3,29 +3,30 @@ const BikesData = require("../models/BikesData");
 const asyncHandler = require('express-async-handler')
 
 const getExactTime = asyncHandler(async (req, res) => {
+   
     // const customer = await AccessCardCustomer.findOne({ creditAccountNumber: req.params.creditAccountNumber }).populate('responses')
     const getWeatherExactDateTime = await WeatherData.find({
         createdAt: {
-          $gte: `2022-06-16T00:00:00.000+00:00`,
-          $lte: `2022-06-16T23:59:59.000+00:00`,
+          $gte: `${req.params.exactdatetime}`,
+          $lte: `${req.params.exactdatetime}`,
+        }
+      })
+      // $gte: `2022-06-16T00:00:00.000+00:00`,
+      // $lte: `2022-06-16T23:59:59.000+00:00`,
+      const getBikesExactDateTime = await BikesData.find({
+        createdAt: {
+          $gte: `${req.params.exactdatetime}`,
+          $lte: `${req.params.exactdatetime}`,
         }
       })
 
-      const getBikesExactDateTime = await BikesData.find({
-        createdAt: {
-          $gte: `2022-06-16T00:00:00.000+00:00`,
-          $lte: `2022-06-16T23:59:59.000+00:00`,
-        }
-      })
-    // if(customer) {
-    //     res.json({
-    //         success: true,
-    //         customer
-    //     })
-    // } else {
-    //     res.status(400)
-    //     throw new Error('Customer not found')
-    // }
+      if(!getWeatherExactDateTime || !getBikesExactDateTime) {
+        res.json({
+          success: true,
+          message: 'No Record found'
+        })
+      }
+
     res.json({
         success: true,
         getWeatherExactDateTime,
--- routes/fetchDataRoutes.js
+++ routes/fetchDataRoutes.js
@@ -5,7 +5,7 @@ const { getExactTime } = require('../controllers/fetchDataController')
 
 const router = express.Router()
 
-router.route('/exacttime')
+router.route('/exacttime/:exactdatetime')
     .get(getExactTime)
 
 module.exports = router
